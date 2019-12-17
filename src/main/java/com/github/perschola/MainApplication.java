14509ea7034eb222b40db7d8a7a59b0195a1d6dd
perscholas-lhunter <lhunter@perscholas.org>
2019-12-17 14:37:38 -0500

    update

--- src/main/java/com/github/perschola/MainApplication.java
+++ src/main/java/com/github/perschola/MainApplication.java
@@ -2,7 +2,14 @@ package com.github.perschola;
 
 public class MainApplication {
     public static void main(String[] args) {
-        Runnable myObject = new MyObject();
-        myObject.run();
+        Person person1 = new Student();
+        Person person2 = new Teacher();
+
+        doSomething(person1);
+        doSomething(person2);
+    }
+
+    public static void doSomething(Person person) {
+        // does a bunch of shit
     }
 }
